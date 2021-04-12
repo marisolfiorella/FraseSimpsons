319616995cd68ba870138a8e2a6f4dc5f10edadc
Marisol Fiorella <marisolfiorellasan@gmail.com>
2021-04-12 16:04:06 -0300

    Se agrego la card y el spinner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Frase from './components/Frase'
+import {useState, useEffect} from 'react';
+import Spinner from "./components/Spinner";
 
 function App() {
+  //creo el state
+  const [personaje, setPersonaje] = useState({});
+  const [cargando, setCargando] = useState(false);
+  useEffect (()=>{
+    //logica a ejecutar
+    consultarApi();
+  },[]);
+  const consultarApi = async()=>{
+    setCargando(true);
+    const respuesta =  await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
+    const resultado = await respuesta.json();
+    console.log(respuesta);
+    console.log(resultado[0]);
+    //guardar objeto dentro del state
+    setPersonaje(resultado[0]);
+    setCargando(false);
+  }
+  //operador ternario (condicion) ? ( lo que quiero hacer si la condicion es verdadera) : (lo que quiero que haga mi programa si la condicion es falsa )
+  const mostrarComponente = cargando === true ? ( <Spinner></Spinner> ) : (<Frase personaje={personaje}></Frase>);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='container text-center my-5'>
+      <article className='d-flex flex-column align-items-center'>
+      <img src={process.env.PUBLIC_URL+ 'logo.png'} alt="Logo de los Simpsons" className='w-75'/>
+      <Button variant="warning" className='shadow my-5' onClick={()=>consultarApi()}>Obtener Frase</Button>
+      </article>
+      {
+        mostrarComponente
+      }
+      {/* <Frase personaje={personaje}></Frase>  
+      <Spinner></Spinner> */}
+    </section>
   );
 }
 
---
+++ src/components/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './spinner.css'
+
+const Spinner = () => {
+    return (
+        <div className="sk-chase container">
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+            <div className="sk-chase-dot"></div>
+</div>
+    );
+};
+
+export default Spinner;
